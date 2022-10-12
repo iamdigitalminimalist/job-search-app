68f12935158ce1c27ee1105c94233dd8945a3149
Or Druker <or@druker.net>
2022-10-12 19:22:24 +0300

    add basic register controller

--- apps/api/src/controllers/authController.ts
+++ apps/api/src/controllers/authController.ts
@@ -1,7 +1,13 @@
 import { Response, Request } from 'express';
+import User from '../model/User';
 
-export const register = (req: Request, res: Response) => {
-  res.send('register user');
+export const register = async (req: Request, res: Response) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json({ user });
+  } catch (err) {
+    res.status(500).json({ msg: `there was an error: ${err}` });
+  }
 };
 export const login = (req: Request, res: Response) => {
   res.send('login');
