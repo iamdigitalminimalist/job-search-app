656e7d6061fbfc247f4ede0742fd9d26871f00ee
Or Druker <or@druker.net>
2022-10-13 10:07:32 +0300

    refactor the error classes

--- apps/api/src/controllers/authController.ts
+++ apps/api/src/controllers/authController.ts
@@ -3,6 +3,12 @@ import { StatusCodes } from 'http-status-codes';
 import User from '../model/UserSchema';
 
 class CustomAPIError extends Error {
+  constructor(message) {
+    super(message);
+  }
+}
+
+class BadRequestError extends CustomAPIError {
   private statusCode: StatusCodes;
   constructor(message) {
     super(message);
@@ -10,10 +16,18 @@ class CustomAPIError extends Error {
   }
 }
 
+class NotFoundError extends CustomAPIError {
+  private statusCode: StatusCodes;
+  constructor(message) {
+    super(message);
+    this.statusCode = StatusCodes.NOT_FOUND;
+  }
+}
+
 export const register = async (req: Request, res: Response) => {
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
-    throw new CustomAPIError('Please provide all values');
+    throw new BadRequestError('Please provide all values');
   }
 
   const user = await User.create({ name, email, password });
