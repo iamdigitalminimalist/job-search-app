62ee472e721064dec42a2504e15339c487a20397
Or Druker <or@druker.net>
2022-10-15 09:49:23 +0300

    get the bearer token

--- apps/api/src/middleware/auth.ts
+++ apps/api/src/middleware/auth.ts
@@ -1,6 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
 
 const auth = async (req: Request, res: Response, next: NextFunction) => {
+  const header = req.headers;
+  const authHeader = req.headers.authorization;
+  console.log(header);
+  console.log(authHeader);
   console.log('authenticate user');
   next();
 };
